d1830d0c4a4f7b48a47cec5447972409338bd6c5
amir-reza <amir-reza@arash.com>
2019-12-19 22:18:57 +0330

    [add] search and searching and showproducts

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('show_basket', views.get_basket.as_view(), name="show_basket"),
     re_path('^product/(?P<id>\d+)$',views.ProductDetailView.as_view() , name = "product_detail"),
-    re_path('^product/(?P<id>\d+)/images$',views.ProductImages.as_view() , name = "product_images")
-
+    re_path('^product/(?P<id>\d+)/images$',views.ProductImages.as_view() , name = "product_images"),
+    path('search/<str:searched>/', views.search, name='search'),
+    path('searching/<str:searched>/', views.searching, name='searching'),
+    path('categoryProducts/<str:cat>/',views.showproducts,name="show_products"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,19 +1,20 @@
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from . import models
 from . import serializers
+from rest_framework.decorators import api_view
+from itertools import chain
+from rest_framework import pagination
+
 class get_basket(APIView):
-
     def get(self, request, *args, **kwargs):
-            exist=True
-            list1=[{"goods_id":13, "number":5 ,"sum":13000} , {"goods_id":14, "number":3 , "sum":75000}]
+        exist = True
+        list1 = [{"goods_id": 13, "number": 5, "sum": 13000}, {"goods_id": 14, "number": 3, "sum": 75000}]
 
-            # exist=False
-            # list1=[]
-            return Response({"exist":exist ,"content":list1})
-
+        # exist=False
+        # list1=[]
+        return Response({"exist": exist, "content": list1})
 
 
 class ProductDetailView(generics.RetrieveAPIView):
@@ -29,3 +30,39 @@ class ProductImages(generics.ListAPIView):
     lookup_field = "product__id"
 
 
+@api_view(['GET'])
+def search(request, searched):
+    products = models.Product.objects.filter(
+        name__contains=searched).order_by('recordTime')[0:10]
+    serializer = serializers.ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def searching(request, searched):
+    searched_length = len(searched)
+    products = models.Product.objects.filter(
+        name__contains=searched).order_by('recordTime')
+
+    for k in range(0, searched_length + 1):
+        products_length = len(products)
+
+        if products_length > 2:
+
+            serializer = serializers.ProductSerializer(products[0:2], many=True)
+            return Response(serializer.data)
+
+
+        else:
+            new_products = models.Product.objects.filter(
+                name__contains=searched[0:searched_length - k - 1]).order_by('recordTime')
+            products = list(chain(products, new_products))
+
+@api_view(['GET'])
+def showproducts(request,cat):
+    paginator = pagination.PageNumberPagination()
+    paginator.page_size = 2
+    product_list=models.Product.objects.all().filter(category=cat)
+    result_page = paginator.paginate_queryset(product_list, request)
+    serializer = serializers.ProductSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
